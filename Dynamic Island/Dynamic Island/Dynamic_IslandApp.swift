97b3b05cae019d7c42c6e64ce60047f0f50de837
Elaidzha1940 <eli080293@gmail.com>
2023-05-09 11:53:52 +0700

    Dynamic Island

---
+++ Dynamic Island/Dynamic Island/Dynamic_IslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_IslandApp.swift
+//  Dynamic Island
+//
+//  Created by Elaidzha Shchukin on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_IslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
